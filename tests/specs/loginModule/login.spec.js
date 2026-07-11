2cf3cad7cdaef9b09af55fedd1ff07eb759a69f7
shifat124 <shifatlaskor779@gmail.com>
2026-07-12 02:58:47 +0600

    commited TC_LF_001 in shifat branch for login functiomality

--- tests/specs/loginModule/login.spec.js
+++ tests/specs/loginModule/login.spec.js
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../../../tests/pageObjects/loginPage/login.page';
 import LoginData from '../../../tests/data/loginData/login.data.json' assert { type: 'json' };
-
 test.describe('Login Functionality', () => {
     test('TC_LF_001 - Validate login to the application using valid credentials', async ({ page }) => {
         const loginPageObject = new LoginPage(page);
         const actual = await loginPageObject.verifyLogin(LoginData.user_name, LoginData.user_password);
-        // console.log('actual', actual);
-        // expect(actual).toBeTruthy();
+        console.log('actual', actual);
+        expect(actual).toBe(LoginData.profile_name);
     });
 });
--- tests/specs/urlModule/url.spec.js
+++ tests/specs/urlModule/url.spec.js
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 import HomePage from '../../../tests/pageObjects/homePage/home.page';
 test.describe('URL Navigation', () => {
     test('TC_UN_01 - Validate searching the target URL and open the webpage', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const actual = await homePage.navigateToHomePage();
+        const homePageObject = new HomePage(page);
+        const actual = await homePageObject.navigateToHomePage();
         console.log('actual', actual);
         expect(actual).toBeTruthy();
     });
