6e5bea3700c23922ae1b67b82c7477ab89cd737f
hasibrahman2020 <rahmanhasib2020@gmail.com>
2026-07-11 23:07:46 +0600

    Writing script for login module

--- tests/pageObjects/homePage/home.page.js
+++ tests/pageObjects/homePage/home.page.js
@@ -3,6 +3,7 @@ class HomePage {
     constructor(page) {
         this.page = page;
         this.darazLogo = page.locator("img[alt='Online Shopping Daraz Logo']");
+        this.loginButton = page.locator("body > div:nth-child(10) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > a:nth-child(1)");
     }
     async navigateToHomePage() {
         const testConfigPageObject = new TestConfig();
