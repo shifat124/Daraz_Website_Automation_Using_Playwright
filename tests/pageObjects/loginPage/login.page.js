053991b78c4a45e2a045ed87e01dd614ac52766e
hasibrahman2020 <rahmanhasib2020@gmail.com>
2026-07-25 01:53:57 +0600

    Wrote script for Login Functionality for TC_LF_003, TC_LF_004 cases

--- tests/pageObjects/loginPage/login.page.js
+++ tests/pageObjects/loginPage/login.page.js
@@ -6,8 +6,10 @@ class LoginPage {
         this.userNameField = page.getByRole('textbox', { name: 'Please enter your Phone or Email' });
         this.userPasswordField = page.getByRole('textbox', { name: 'Please enter your password' });
         this.loginButton = page.locator('div.iweb-button-mask');
+        this.invalidLoginMessage = page.getByText('Invalid account or password.', { exact: true });
+        this.crossIcon = page.locator("//div[contains(@class,'iweb-dialog-container-enter')]//div[contains(@class,'lzd-member-loginsign-popup-close-button')]//div//*[name()='svg']//*[name()='path' and contains(@d,'M28 8 8 28')]");
     }
-    async verifyLogin(username, password) {
+    async verifyValidLogin(username, password) {
         const testConfigPageObject = new TestConfig();
         await this.page.goto(testConfigPageObject.baseUrl);
         const homePageObject = new HomePage(this.page);
@@ -20,5 +22,31 @@ class LoginPage {
         console.log('profile', profile);
         return profile;
     }
+    async verifyInvalidLogin(username, password) {
+        const testConfigPageObject = new TestConfig();
+        await this.page.goto(testConfigPageObject.baseUrl);
+        const homePageObject = new HomePage(this.page);
+        await homePageObject.loginLink.click();
+        await this.userNameField.fill(username);
+        await this.userPasswordField.fill(password);
+        await this.loginButton.click();
+        // const invalidLoginMessageVisible = await this.invalidLoginMessage.isVisible();
+        // console.log('invalidLoginMessageVisible', invalidLoginMessageVisible);
+        // return invalidLoginMessageVisible;
+        await this.crossIcon.click();
+        const loginLink = await homePageObject.loginLink.isVisible();
+        console.log('loginLink', loginLink);
+        return loginLink;
+    }
 }
 export default LoginPage;
+
+
+
+
+
+
+
+
+
+
