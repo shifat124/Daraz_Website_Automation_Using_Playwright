43227467c66bcb1799c894f8ad84f2683d7bf38d
shifat124 <shifatlaskor779@gmail.com>
2026-08-15 00:38:37 +0600

    added TC_LG_002 case

--- tests/pageObjects/logoutPage/logout.page.js
+++ tests/pageObjects/logoutPage/logout.page.js
@@ -12,12 +12,36 @@ class LogoutPage {
         const loginPageObject = new LoginPage(this.page);
         await loginPageObject.login(username, password);
         const homePageObject = new HomePage(this.page);
-        //await homePageObject.accountName.waitFor({ state: 'visible' });
         await homePageObject.accountName.click();
         await this.logoutButton.click();
+        await homePageObject.loginLink.waitFor({ state: 'visible' });
         const logoutLink = await homePageObject.loginLink.isVisible();
         console.log("logoutLink", logoutLink);
         return logoutLink;
+    }
+    async verifyAutomaticLogout(username, password, browser) {
+        const context1 = await browser.newContext();
+        const page1 = await context1.newPage();
+        const testConfigPageObject = new TestConfig();
+        await page1.goto(testConfigPageObject.baseUrl);
+        const loginPageObject = new LoginPage(page1);
+        await loginPageObject.login(username, password);
+        const homePageObjectFirstContext = new HomePage(page1);
+        await homePageObjectFirstContext.accountName.waitFor({ state: 'visible' });
+        const accountNameVisible = await homePageObjectFirstContext.accountName.isVisible();
+        console.log("accountNameVisible", accountNameVisible);
+        if (accountNameVisible === true) {
+            await context1.close();
+            const context2 = await browser.newContext();
+            const page2 = await context2.newPage();
+            const testConfigPageObject = new TestConfig();
+            await page2.goto(testConfigPageObject.baseUrl);
+            const homePageObjectSecondContext = new HomePage(page2);
+            await homePageObjectSecondContext.loginLink.waitFor({ state: 'visible' });
+            const loginLinkVisible = await homePageObjectSecondContext.loginLink.isVisible();
+            console.log("loginLinkVisible", loginLinkVisible);
+            return loginLinkVisible;
+        }
     }
 }
 export default LogoutPage;
--- tests/specs/logoutModule/logout.spec.js
+++ tests/specs/logoutModule/logout.spec.js
@@ -3,10 +3,16 @@ import LogoutPage from "../../../tests/pageObjects/logoutPage/logout.page";
 import LoginData from "../../data/loginData/login.data.json" assert { type: 'json' };
 
 test.describe('Logout Functionality', () => {
-    test('Validate Logging out by selecting Logout option  from "My Account" dropdown menu', async ({ page }) => {
+    test('TC_LG_001 - Validate Logging out by selecting Logout option  from "My Account" dropdown menu', async ({ page }) => {
         const logoutPageObject = new LogoutPage(page);
         const actual = await logoutPageObject.verifyLogout(LoginData.valid_email, LoginData.valid_password);
-        console.log("actual", actual)
+        console.log("actual", actual);
+        expect(actual).toBeTruthy();
+    });
+    test.only('TC_LG_002 - Validate automatic logout after closing the browser without logging out', async ({ page, browser }) => {
+        const logoutPageObject = new LogoutPage(page);
+        const actual = await logoutPageObject.verifyAutomaticLogout(LoginData.valid_email, LoginData.valid_password, browser);
+        console.log("actual", actual);
         expect(actual).toBeTruthy();
     });
 });
